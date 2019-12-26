494d5ad6d7d9f72b413d27525944fa75638eceaf
Arish-Shah <arish0246@gmail.com>
2019-12-26 21:21:43 +0530

    async scripts

--- 1. The Unconventional Calculator/assets/scripts/app.js
+++ 1. The Unconventional Calculator/assets/scripts/app.js
@@ -1,13 +1,62 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add(a, b) {
-  return a + b;
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-const additionResult = add(1, 2);
-currentResult = additionResult;
+// Generates and writes calculation log
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
-let calculationDescription = `(${defaultResult} + 10)`;
+function writeToLog(operation, prevResult, number, result) {
+  const logEntry = {
+    operation,
+    prevResult,
+    number,
+    result
+  };
 
-outputResult(currentResult, calculationDescription);
+  logEntries.push(logEntry);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteLog('+', initialResult, enteredNumber);
+  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteLog('-', initialResult, enteredNumber);
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteLog('*', initialResult, enteredNumber);
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteLog('/', initialResult, enteredNumber);
+  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
