5d09a63ca9879024ecc505e5fd7ab90014b0ef5b
Arish-Shah <arish0246@gmail.com>
2020-01-23 10:54:03 +0530

    wc

--- 5. DOM Interaction/app.js
+++ 5. DOM Interaction/app.js
@@ -1,3 +1,38 @@
-function log(msg) {
-  console.log(msg);
-}
+window.customElements.define(
+  'x-component',
+  class extends HTMLElement {
+    connectedCallback() {
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.innerHTML = `
+        <h1>Welcome to Web Components!</h1>
+        <button></button>
+      `;
+
+      this.button = this.shadowRoot.querySelector('button');
+      this.button.addEventListener('click', () => {
+        this.count = this.count + 1;
+      });
+      this.count = 0;
+    }
+
+    get count() {
+      return +this.getAttribute('count');
+    }
+
+    set count(val) {
+      this.setAttribute('count', val);
+    }
+
+    static get observedAttributes() {
+      return ['count'];
+    }
+
+    attributeChangedCallback() {
+      this.update();
+    }
+
+    update() {
+      this.button.textContent = `Counts: ${this.count}`;
+    }
+  }
+);
