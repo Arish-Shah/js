8e437b369e337ba6a9da25f75883264aade5a889
Arish-Shah <arish0246@gmail.com>
2020-02-21 21:25:37 +0530

    --

--- 7. Arrays and Iterables/app.js
+++ 7. Arrays and Iterables/app.js
@@ -8,3 +8,13 @@ hobbies.splice(1, 0, 'Running', 'Hunting');
 const newHobbies = hobbies.slice();
 
 console.log(newHobbies);
+
+function h(type, props, ...children) {
+  return {
+    type,
+    props,
+    children
+  };
+}
+
+console.log(h('h1', { id: 'title' }, 'Heading 1'));
