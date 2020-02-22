eccc287ac8a01c79d5b83d6b7cdea769ea5ce816
Arish-Shah <arish0246@gmail.com>
2020-02-22 21:24:06 +0530

    --

--- 7. Arrays and Iterables/app.js
+++ 7. Arrays and Iterables/app.js
@@ -5,5 +5,6 @@ hobbies.pop();
 hobbies.unshift('Boating');
 hobbies.shift();
 hobbies.splice(1, 0, 'Running', 'Hunting');
-console.log(hobbies);
 const newHobbies = hobbies.slice();
+
+console.log(newHobbies);
