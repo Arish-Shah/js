f237c2a85680e405e199eac41342330a28c5031e
Arish-Shah <arish0246@gmail.com>
2020-02-24 19:26:12 +0530

    --

--- 7. Arrays and Iterables/app.js
+++ 7. Arrays and Iterables/app.js
@@ -1,48 +1,7 @@
-const hobbies = ['Sports', 'Gaming'];
+const numbers = [1, 2, 3, 4];
 
-hobbies.push('Swimming');
-hobbies.pop();
-hobbies.unshift('Boating');
-hobbies.shift();
-hobbies.splice(1, 0, 'Running', 'Hunting');
-const newHobbies = hobbies.slice();
+const sum = numbers.reduce((prevValue, currentValue) => {
+  return prevValue + currentValue;
+}, 0);
 
-console.log(newHobbies);
-
-class XComponent extends HTMLElement {
-  connectedCallback() {
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      <input type="text" />
-      <h1></h1>
-    `;
-    this.input = this.shadowRoot.querySelector('input');
-    this.h1 = this.shadowRoot.querySelector('h1');
-
-    this.input.addEventListener('input', e => {
-      this.message = e.target.value;
-    });
-  }
-
-  static get observedAttributes() {
-    return ['message'];
-  }
-
-  attributeChangedCallback() {
-    this.update();
-  }
-
-  get message() {
-    return this.getAttribute('message');
-  }
-
-  set message(val) {
-    this.setAttribute('message', val);
-  }
-
-  update() {
-    this.h1.textContent = this.message;
-  }
-}
-
-customElements.define('x-component', XComponent);
+console.log(sum);
