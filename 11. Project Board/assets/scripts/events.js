37a9c615998f40798d48d1f9dc4afd28915a99d0
Arish-Shah <arish0246@gmail.com>
2020-05-30 19:48:58 +0530

    add events

--- 11. Project Board/assets/scripts/events.js
+++ 11. Project Board/assets/scripts/events.js
@@ -1,11 +1,24 @@
 let buttons = document.querySelectorAll('button');
 
 const buttonClickHandler = event => {
+  console.log(event);
   event.target.disabled = true;
 };
 
-buttons.forEach(button => button.addEventListener('click', buttonClickHandler));
+const anotherButtonClickHandler = () => {
+  console.log('This was clicked');
+};
+
+const boundFn = buttonClickHandler.bind(this);
+
+buttons.forEach(button => {
+  button.addEventListener('click', buttonClickHandler);
+});
 
 // setTimeout(() => {
 //   button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
+
+window.addEventListener('scroll', event => {
+  console.log(event);
+});
