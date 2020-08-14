e69d5d5b5e0bb3159c88c3a111ad5d1c6cb50462
Arish Shah <arish0246@gmail.com>
2020-08-14 23:06:07 +0530

    use fetch

--- 15-http/assets/scripts/app.js
+++ 15-http/assets/scripts/app.js
@@ -5,40 +5,45 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.responseType = 'json';
-
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.response);
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
       } else {
-        reject(new Error('Something went wrong'));
+        return response.json().then(errData => {
+          console.log(errData);
+          throw new Error('Something went wrong - server-side.');
+        });
       }
-    };
-
-    xhr.onerror = function () {
-      reject(new Error('Failed to send request'));
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
+    })
+    .catch(error => {
+      console.log(error);
+      throw new Error('Error occured');
+    });
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/pots'
-  );
-  const posts = responseData;
-  for (const post of posts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title;
-    postEl.querySelector('p').textContent = post.body;
-    postEl.querySelector('li').id = post.id;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    const posts = responseData;
+    for (const post of posts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title;
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (err) {
+    alert(err.message);
   }
 }
 
