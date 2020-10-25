311eb13140864dbd6537ae05b339a5fc7f3b8091
Arish Shah <arish0246@gmail.com>
2020-10-25 20:35:04 +0530

    finish project

--- 20-places/src/SharePlace.js
+++ 20-places/src/SharePlace.js
@@ -1,12 +1,46 @@
 import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
+import { getAddressFromCoords, getCoordsFromAddress } from './Utility/Location';
 
 class PlaceFinder {
   constructor() {
-    const form = document.querySelector('form');
+    const addressForm = document.querySelector('form');
     const locateUserBtn = document.getElementById('locate-btn');
+    this.shareBtn = document.getElementById('share-btn');
+    this.shareLinkInputElement = document.getElementById('share-link');
 
-    locateUserBtn.addEventListener('click', this.locateUserHandler);
-    form.addEventListener('submit', this.findAddressHandler);
+    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener('click', this.sharePlaceHandler.bind(this));
+    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+  }
+
+  sharePlaceHandler() {
+    if (!navigator.clipboard) {
+      this.shareLinkInputElement.select();
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(this.shareLinkInputElement.value)
+      .then(() => alert('Copied to clipboard'))
+      .catch(error => {
+        console.log(error);
+        this.shareLinkInputElement.select();
+      });
+  }
+
+  selectPlace(coords, address) {
+    if (this.map) {
+      this.map.render(coords);
+    }
+    this.map = new Map(coords);
+
+    this.shareBtn.disabled = false;
+    this.shareLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coords.lat}&lng=${
+      coords.lng
+    }`;
   }
 
   locateUserHandler() {
@@ -23,13 +57,14 @@ class PlaceFinder {
     );
     modal.show();
     navigator.geolocation.getCurrentPosition(
-      successResult => {
-        modal.hide();
+      async successResult => {
         const coords = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude
         };
-        console.log(coords);
+        const address = await getAddressFromCoords(coords);
+        modal.hide();
+        this.selectPlace(coords, address);
       },
       error => {
         modal.hide();
@@ -40,7 +75,29 @@ class PlaceFinder {
     );
   }
 
-  findAddressHandler() {}
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if (!address || address.trim().length === 0) {
+      alert('Invalid address entered - please try again!');
+      return;
+    }
+
+    const modal = new Modal(
+      'loading-modal-content',
+      'Loading location - please wait!'
+    );
+    modal.show();
+
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+
+    modal.hide();
+  }
 }
 
-new PlaceFinder();
+const placeFinder = new PlaceFinder();
---
+++ 20-places/src/Utility/Location.js
@@ -0,0 +1,10 @@
+export async function getCoordsFromAddress() {
+  return {
+    lat: 51.5,
+    lng: 0.12
+  };
+}
+
+export async function getAddressFromCoords(coords) {
+  return 'Raipur, Chhattisgarh';
+}
