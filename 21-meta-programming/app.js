e741b44bb3811e15cc855a3a2325e444c34c0510
Arish Shah <arish0246@gmail.com>
2020-10-26 18:13:05 +0530

    add perf

--- 21-meta-programming/app.js
+++ 21-meta-programming/app.js
@@ -1,51 +1,76 @@
-// const uid = Symbol('uid');
-// console.log(uid);
+// // const uid = Symbol('uid');
+// // console.log(uid);
 
-// const user = {
-//   [uid]: 'p1',
-//   name: 'Arish',
-//   age: 22,
-//   [Symbol.toStringTag]: 'User'
+// // const user = {
+// //   [uid]: 'p1',
+// //   name: 'Arish',
+// //   age: 22,
+// //   [Symbol.toStringTag]: 'User'
+// // };
+
+// // user[uid] = 'p3';
+
+// // user.id = 'p2';
+
+// // console.log(user[Symbol('uid')]);
+// // console.log(Symbol('uid') === Symbol('uid'));
+// // console.log(user.toString());
+
+// const company = {
+//   currEmployee: 0,
+//   employees: ['Arish', 'Rahil', 'Shah'],
+//   next() {
+//     if (this.currEmployee >= this.employees.length) {
+//       return { value: this.currEmployee, done: true };
+//     }
+
+//     const returnValue = {
+//       value: this.employees[this.currEmployee],
+//       done: false
+//     };
+//     this.currEmployee++;
+//     return returnValue;
+//   },
+//   [Symbol.iterator]: function* employeeGenerator() {
+//     let currEmployee = 0;
+//     while (currEmployee < this.employees.length) {
+//       yield this.employees[currEmployee];
+//       currEmployee++;
+//     }
+//   }
 // };
 
-// user[uid] = 'p3';
+// // console.log(company.next());
+// // console.log(company.next());
+// // console.log(company.next());
+// // console.log(company.next());
+// // console.log(company.next());
 
-// user.id = 'p2';
+// for (let employee of company) {
+//   console.log(employee);
+// }
 
-// console.log(user[Symbol('uid')]);
-// console.log(Symbol('uid') === Symbol('uid'));
-// console.log(user.toString());
+const course = {
+  title: 'Arish Rahil Shah'
+};
 
-const company = {
-  currEmployee: 0,
-  employees: ['Arish', 'Rahil', 'Shah'],
-  next() {
-    if (this.currEmployee >= this.employees.length) {
-      return { value: this.currEmployee, done: true };
-    }
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title;
+  }
+});
 
-    const returnValue = {
-      value: this.employees[this.currEmployee],
-      done: false
-    };
-    this.currEmployee++;
-    return returnValue;
-  },
-  [Symbol.iterator]: function* employeeGenerator() {
-    let currEmployee = 0;
-    while (currEmployee < this.employees.length) {
-      yield this.employees[currEmployee];
-      currEmployee++;
-    }
+// -- Proxy --
+
+const courseHandler = {
+  get(obj, propertyName) {
+    console.log(propertyName);
+    return obj[propertyName] || 'Not Found';
   }
 };
 
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
-// console.log(company.next());
+const pCourse = new Proxy(course, courseHandler);
 
-for (let employee of company) {
-  console.log(employee);
-}
+console.log(pCourse.title);
+console.log(course, pCourse);
+console.log(pCourse.foo);
