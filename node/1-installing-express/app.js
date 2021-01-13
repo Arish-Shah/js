1d5a7f906cc086416d0bd20be574d982acd83875
Arish Shah <arish0246@gmail.com>
2021-01-13 23:39:24 +0530

    add views

--- node/1-installing-express/app.js
+++ node/1-installing-express/app.js
@@ -1,28 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// This always runs
-app.use("/", (req, res, next) => {
-  next();
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form method="POST" action="/product"><input type="text" name="title" /><button type="submit">Add</button></form>'
-  );
-});
-
-app.use("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(8080);
