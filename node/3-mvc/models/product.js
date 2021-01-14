a594e09bc69141ac1abefcf7e293276da30189ff
Arish Shah <arish0246@gmail.com>
2021-01-14 13:42:57 +0530

    Add MVC

---
+++ node/3-mvc/models/product.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "..", "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(data));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
+  }
+};
