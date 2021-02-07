35b00d770db0c358b76640448c723b45a4e2b7de
Arish-Shah <arish0246@gmail.com>
2021-02-07 19:17:36 +0530

    remove graphql

--- graphql/typescript-example/server/src/resolvers/Todo.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "../Context";
-import { TodoResolvers, User } from "../generated/graphql";
-
-export const Todo: TodoResolvers = {
-  creator: async (parent, _: any, { User }: Context): Promise<User> => {
-    const userId = parent.creator._id;
-    return (await User.findById(userId)) as User;
-  }
-};
